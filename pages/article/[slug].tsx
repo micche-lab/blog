f2b4a69b350036a3fa3d05beca1fdf048ee0f150
micche <77561443 +micche-lab@users.noreply.github.com>
2023-06-20 08:54:12 +0900

    記事詳細ページ編集

--- pages/article/[slug].tsx
+++ pages/article/[slug].tsx
@@ -140,7 +140,7 @@ export default function ArticlePage({
               {currentArticle.tags.map((tag) => (
                 <li key={tag._id}>
                   <Link href={`/tag/${tag.slug}`}>
-                    <a>タグ:{tag.name}</a>
+                    <a>#{tag.name}</a>
                   </Link>
                 </li>
               ))}
@@ -178,7 +178,7 @@ export default function ArticlePage({
                 </div>
               </div>
               <div className={styles.Article_Share}>
-                <p className={styles.Article_ShareLabel}>Share this post</p>
+                <p className={styles.Article_ShareLabel}>記事の共有</p>
                 <ul className={styles.Article_ShareList}>
                   <li>
                     <button type="button" onClick={shareOnTwitter}>
@@ -215,7 +215,7 @@ export default function ArticlePage({
             dangerouslySetInnerHTML={body}
           ></div>
           <div className={styles.SnsShare}>
-            <p className={styles.SnsShare_Label}>Share this post</p>
+            <p className={styles.SnsShare_Label}>共有</p>
             <ul className={styles.SnsShare_List}>
               <li>
                 <button type="button" onClick={shareOnTwitter}>
@@ -291,14 +291,14 @@ export default function ArticlePage({
                     <path d="M0 0h24v24H0V0z" fill="none" opacity=".87" />
                     <path d="M17.51 3.87L15.73 2.1 5.84 12l9.9 9.9 1.77-1.77L9.38 12l8.13-8.13z" />
                   </svg>
-                  Previous post
+                  前へ
                 </a>
               </Link>
             )}
             {nextArticle && (
               <Link href={`/article/${nextArticle.slug}`}>
                 <a className={styles.Links_Next}>
-                  Next post
+                  次へ
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     enableBackground="new 0 0 24 24"
