947b011a54d6e700370e51f6233c02d2979cc2f0
micche <77561443 +micche-lab@users.noreply.github.com>
2023-06-23 17:53:05 +0900

    目次モジュール追加

---
+++ src/components/Toc.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react' //レンダリング後に行う関数
+import tocbot from 'tocbot' //tocbotという名前で使えるようにする
+import styles from '@/styles/Toc.module.css'
+
+export default function TocBot(){
+  useEffect(() => {
+    tocbot.init({
+      tocSelector: '.toc', //目次表示させる要素のクラス名
+      contentSelector: '.post', //目次を抽出したい要素のクラス名
+      headingSelector: 'h2, h3', //抽出先見出しタグ
+    })
+
+    return () => tocbot.destroy()
+  }, [])
+
+  return (
+    <div className="{styles.toc_Header}">
+    <span>目次</span>
+    <div className="{styles.toc}">
+      
+    </div>
+    </div>
+  )
+}
